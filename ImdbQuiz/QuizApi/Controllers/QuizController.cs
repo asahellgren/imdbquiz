bbb3639ff9e802a2b14f37770637ea0c4eabcee3
asahellgren <asa@hellosugar.se>
2015-11-19 19:31:51 +0100

    quiz api added

---
+++ ImdbQuiz/QuizApi/Controllers/QuizController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AttributeRouting.Web.Http;
+using QuizBLL;
+using Repositories;
+
+namespace QuizApi.Controllers
+{
+    public class QuizController : ApiController
+    {
+
+        private Movie _randomMovie;
+
+        // GET: api/Quiz/RandomMovie
+        [HttpGet]
+        [GET("Quiz/GetRandomMovie/")]
+        public string GetRandomMovie()
+        {
+            _randomMovie = new RandomMovie().GetRandomMovie();
+            return _randomMovie.Title;
+        }
+       
+        // GET: api/Quiz/RandomMovie
+        [HttpGet]
+        [GET("Quiz/GetRandomMovie/{year}")]
+        public int GetRandomMovie(string year)
+        {
+            return _randomMovie.Year == year ? 1 : 0;
+        }
+
+        // POST: api/Quiz
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Quiz/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Quiz/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
