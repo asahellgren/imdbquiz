67f7391240ce126e4aebddf9922af56fcb60d7ff
asahellgren <asa@hellosugar.se>
2015-11-19 15:38:04 +0100

    EF update

--- ImdbQuiz/Repositories/ImdbRepository.cs
+++ ImdbQuiz/Repositories/ImdbRepository.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ImdbQuiz;
 
 namespace Repositories
 {
     public class ImdbRepository
     {
+        private readonly ImdbContext _context;
     }
 }
